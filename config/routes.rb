59f7a988f05168d930b38babefe59a382f324484
ryan <ball.ryan.rb@gmail.com>
2021-05-05 11:52:26 -0600

    updates contacts database

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/first_contact", controller: "contacts", action: "first_contact"
-  get "/all_contacts", controller: "contacts", action: "all_contacts"
+
+  # get "/first_contact", controller: "contacts", action: "first_contact"
+  # get "/all_contacts", controller: "contacts", action: "all_contacts"
+
+  # ROUTE SHORTHAND
+  get "first_contact" => "contacts#first_contact"
+  get "all_contacts" => "contacts#all_contacts"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Contact.create(first_name: "Eric", last_name: "Horst", email: "erichorst@t4c.com", phone_number: "478-999-3255")
+
+Contact.create(first_name: "Alex", last_name: "Puccio", email: "thepucc@gmail.com", phone_number: "262-658-7844")
+
+Contact.create(first_name: "Margo", last_name: "Hayes", email: "margo@aol.com", phone_number: "303-245-5388")
+
+Contact.create(first_name: "Adam", last_name: "Ondra", email: "adamondra@me.com", phone_number: "445-875-3378")
